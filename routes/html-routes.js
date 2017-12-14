998bf20c5c265a19c46744090e8eaeead3c4cf6b
Melanie MacDonald <melmacd94@gmail.com>
2017-12-14 13:34:00 -0500

    passport with html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,3 +1,26 @@
+var path = require("path");
+
+var isAuthenticated = require("../config/middleware/isAuthenticated.js");
 module.exports = function(app) {
+  //if user is signed in send to user dash if not redirect to signup
+  app.get("/", function(req, res) {
+    if (req.user) {
+        res.redirect("/members");
+      }
+      res.sendFile(path.join(__dirname, "../public/signup.html"));
+    });
+
+    //if user is a signed in send to user dash else send to login
+    app.get("/login", function(req, res) {
+      if (req.user) {
+        res.redirect("/members");
+      }
+      res.sendFile(path.join(__dirname, "../public/login.html"));
+    });
+
+    //if user is atuhenticated send to user dash
+    app.get("/members", isAuthenticated, function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/members.html"));
+    });
 
 };
