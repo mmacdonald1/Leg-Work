4a137e5e45de930263e8f38e4cde7ff7f779ea23
Anastasia Lucas <ani@theta.local>
2017-12-14 10:25:38 -0500

    db

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,19 +1,25 @@
 var db = require('../models');
 var express = require('express');
-var router  = express.Router();
+var router = express.Router();
 
 var passport = require("../config/passport");
 
 module.exports = function(app) {
+    //get route for getting all the applications
+    router.get('/api/apps', function(req, res) {
+        var query = {};
+        if (req.query.user_id) {
+          query.UserId = req.query.user_id
+        }
+        // Here we add an include property to our options in our findAll query
+        //we set the value to an array of the models we want to include
+        db.Application.findAll({
+          where: query,
+        
+        }).then(function(dbApp) {
+          res.json(dbApp);
+        });
+      };
 
-router.get('/', function(req, res) {
-  models.User.findAll({
-    include: [ models.Application ]
-  }).then(function(users) {
-    res.render('index', {
-      title: 'Applications',
-      users: users
-    });
-  });
-});
-};
+
+    };
