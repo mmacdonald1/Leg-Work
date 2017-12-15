069594b2c94d71080cf254b780a34a9642cb3818
Anastasia Lucas <ani@theta.local>
2017-12-15 10:05:55 -0500

    handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,14 +13,19 @@ module.exports = function(app) {
     //if user is a signed in send to user dash else send to login
     app.get("/login", function(req, res) {
       if (req.user) {
-        res.redirect("/members");
+        res.render("members", { hello: "world"});
       }
       res.sendFile(path.join(__dirname, "../public/login.html"));
     });
 
     //if user is atuhenticated send to user dash
     app.get("/members", isAuthenticated, function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/members.html"));
+      console.log('yo');
+      //res.sendFile(path.join(__dirname, "../public/members.html"));
+      res.render("members", { hello: "world"});
     });
+
+
+
 
 };
--- server.js
+++ server.js
@@ -2,11 +2,16 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var session = require("express-session");
 var passport = require("./config/passport");
-
+const exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
-
 var app = express();
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+//app.use("/static/", express.static(path.join(__dirname, "/public/")));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
