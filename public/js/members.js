fb007d456d460b2e4c5946e0365fc9ae54065512
Melanie MacDonald <melmacd94@gmail.com>
2018-01-25 21:32:10 -0500

    profile

--- public/js/members.js
+++ public/js/members.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
   $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.username);
   });
+
+  // $('.addbtn').on('click', function(event) {
+  //     $('.application-form').toggleClass('hidden');
+  //     $('.table').css('width','100%');
+  //   });
 });
